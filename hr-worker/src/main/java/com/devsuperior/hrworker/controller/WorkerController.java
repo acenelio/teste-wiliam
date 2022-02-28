17f66cfc6034d5bd9135bf457317245c21596544
Wiliam M D'Epiro <wiliam.depiro@hotmail.com>
2022-02-28 13:57:00 -0300

    payrool

--- hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controller/WorkerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,10 @@ public class WorkerController {
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
 		return ResponseEntity.ok().body(service.findAll());
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 
 }
--- hr-worker/src/main/java/com/devsuperior/hrworker/service/WorkerService.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/service/WorkerService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.hrworker.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,9 @@ public class WorkerService {
 	public List<Worker> findAll(){
 		List<Worker> workers = repository.findAll();
 		return workers;
+	}
+	public Worker findById(Long id){
+		Optional<Worker> worker = repository.findById(id);
+		return worker.get();
 	}
 }
